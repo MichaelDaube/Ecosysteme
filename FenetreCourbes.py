eb58e5b806a474393e2b871d15774c6d887dcfc9
PatEcosystem <45672506+PatEcosystem@users.noreply.github.com>
2019-01-06 21:51:31 +0100

    Update FenetreCourbes.py

--- FenetreCourbes.py
+++ FenetreCourbes.py
@@ -3,8 +3,7 @@ import matplotlib.pyplot as plt
 class FenetreCourbe(object):
 	"à partir de listes de valeurs (les tailles des populations relevées à intervalles réguliers) affiche des courbes d'évolution de ces population en fonction du temps"
 	
-	def __init__(self,donnees=[24,58,65,69,32,14,78,25,3,8]):
-		self.donnees=donnees
+	def __init__(self,donnees=[24,58,65,69,32,14,78,25,3,8]):  ## Cette liste ne sert qu'à initialiser, on pourra la changer ou la supprimer
 			
 	
 	def afficheCourbe(self):
@@ -15,5 +14,13 @@ class FenetreCourbe(object):
 		plt.plot(uniteTemps,self.donnees)
 		plt.show()
 		
+####################################################################
+#
+# Pour l'utilisation, on procédera ainsi :
+#
+# f1=FenetreCourbe([31,45,54,50,38]) (par exemple...)
+# f1.afficheCourbe()
+#
+####################################################################
 
 		
